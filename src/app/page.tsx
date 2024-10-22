21431a91355d9a2834f8bde4f576e0e67ac6f7f6
kamal sharma <kamalbsr2101@gmail.com>
2024-10-22 22:34:46 +0530

    home screen is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Home from "@/components/Home";
+import HomeComponents from "@/components/Home"
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Home />
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <HomeComponents/>
     </div>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,20 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import img from "../../public/dice-pic.png"
 
-const Home = () => {
+const HomeComponents = () => {
     return (
         <>
-        <section className="flex item-center justify-center">
-            <div className="">
-                <Image src="dice-pic.png" alt="" />
+        <section className="w-full h-screen gap-16 flex item-center justify-center">
+            <div className="flex justify-center items-center">
+                <Image src={img} alt="" className="w-96"/>
             </div>
-            <div className=""></div>
+            <div className="flex justify-center items-end flex-col gap-5">
+                <h1 className="text-black text-5xl font-extrabold">DICE GAME</h1>
+                <button className="bg-black py-1 w-48 cursor-pointer rounded-md font-semibold text-white">Play Now</button>
+            </div>
         </section>
         </>
     )
 }
-export default Home;
+export default HomeComponents;
