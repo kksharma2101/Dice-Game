4c708b575007e6855be96a82f7a9210eb9824a66
kamal sharma <kamalbsr2101@gmail.com>
2024-10-22 10:16:03 +0530

    first commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <section className="flex item-center justify-center">
+            <div className="">
+                <Image src="dice-pic.png" alt="" />
+            </div>
+            <div className=""></div>
+        </section>
+        </>
+    )
+}
+export default Home;
