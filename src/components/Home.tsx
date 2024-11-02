cad87d0b5dfe37b7315bee31c23eb3bfbcaff405
kamal sharma <kamalbsr2101@gmail.com>
2024-11-02 14:02:25 +0530

    change width

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,12 +5,12 @@ import img from "../../public/dice-pic.png"
 const HomeComponents = () => {
     return (
         <>
-        <section className="w-full h-screen gap-16 flex item-center justify-center">
+        <section className="max-w-[1122px] h-screen gap-16 flex item-center justify-center">
             <div className="flex justify-center items-center">
                 <Image src={img} alt="" className="w-96"/>
             </div>
             <div className="flex justify-center items-end flex-col gap-5">
-                <h1 className="text-black text-5xl font-extrabold">DICE GAME</h1>
+                <h1 className="text-black text-5xl font-extrabold text-nowrap">DICE GAME</h1>
                 <button className="bg-black py-1 w-48 cursor-pointer rounded-md font-semibold text-white">Play Now</button>
             </div>
         </section>
